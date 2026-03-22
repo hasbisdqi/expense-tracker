c57491568ebde13dbe8c69e5da691934d6033901
hasbisdqi <muhhasbiassidiqi18@gmail.com>
2026-03-22 21:55:55 +0700

    Revert to 2ecb3f1

--- src/components/ui/aspect-ratio.tsx
+++ src/components/ui/aspect-ratio.tsx
@@ -1,9 +1,5 @@
-import { AspectRatio as AspectRatioPrimitive } from "radix-ui"
+import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
 
-function AspectRatio({
-  ...props
-}: React.ComponentProps<typeof AspectRatioPrimitive.Root>) {
-  return <AspectRatioPrimitive.Root data-slot="aspect-ratio" {...props} />
-}
+const AspectRatio = AspectRatioPrimitive.Root;
 
-export { AspectRatio }
+export { AspectRatio };
